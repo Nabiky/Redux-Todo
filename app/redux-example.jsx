f8ccf0372a3a681ee4672d6c59026c09fc88770b
Mireya Monzon <marte.pepen.mireya@gmail.com>
2017-03-16 22:57:35 +1300

    Add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,14 +4,32 @@ console.log('Starting redux example');
 
 var reducer = (state ={name: 'Anonymous'}, action) => {
   // state = state || {name: 'Anonymous'};
-  return state;
+
+  // console.log('New action', action);
+   switch (action.type) {
+    case 'CHANGE_NAME':
+     return {
+       ...state,
+       name: action.name
+     };
+     default:
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer); // this createStore method takes one argument
                                        // which needs to be a pure function (reducer).
 
 var currentState = store.getState();
-console.log('currentState', currentState);
+console.log('currentState 1', currentState);
 
 //A reducer takes your existent state and actions as arguments,
-//and return the new state 
+//and return the new state
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Andrew'
+};
+store.dispatch(action);
+
+console.log('Name should be andrew', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,8 +9,26 @@ var stateDefault = {
 };
 
 var reducer = (state = stateDefault, action) => {
-  return state;
-};
+
+   switch(action.type) {
+     case 'CHANGE_SEARCHTEXT':
+         return {
+            ...state,
+            searchText: action.searchText
+          };
+          default:
+            return state;
+
+        }
+  };
 
 var store = redux.createStore(reducer);
 console.log('currentState', store.getState());
+
+var action = {
+   type: 'CHANGE_SEARCHTEXT',
+  searchText: 'Walk'
+};
+
+store.dispatch(action);
+ console.log('SearchText should be "Walk"', store.getState());
