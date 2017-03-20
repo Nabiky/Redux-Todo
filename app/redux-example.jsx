c0a4bfdf96f2ac9293a2dd5c66472765a7d568c4
Mireya Monzon <marte.pepen.mireya@gmail.com>
2017-03-20 13:17:02 +1300

    Convert action generator

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,14 +2,177 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var stateDefault = {
-  name: 'Anonymous',
-  hobbies: [],
-  movies: []
+// Name reducer and action generators
+// -----------------------------------
+var nameReducer = (state = 'Anonymous', action) => {
+  switch (action.type) {
+    case 'CHANGE_NAME':
+    return action.name
+    default:
+    return state;
+  };
 };
-var nextHobbyId = 1;
-var nextMovieId = 1;
 
+var changeName = (name) => {
+  return {
+    type: 'CHANGE_NAME',
+    name    //or the es5 way name:name
+  }
+};
+
+
+
+// Hobbies reducer and action generators
+// -----------------------------------
+var nextHobbyId = 1;
+var hobbiesReducer = (state =[], action) => {
+  switch (action.type) {
+    case 'ADD_HOBBY':
+    return [
+      ...state, // all the existing hobbies.
+      {
+        id: nextHobbyId++,
+        hobby: action.hobby
+      }
+    ];
+    case 'REMOVE_HOBBY':
+      return state.filter((hobby) => hobby.id !== action.id)
+    default:
+      return state;
+  }
+};
+
+var addHobby = (hobby) => {
+  return {
+    type: 'ADD_HOBBY',
+    hobby
+  }
+};
+
+var removeHobby = (id) => {
+  return {
+    type: 'REMOVE_HOBBY',
+    id
+  }
+};
+
+
+
+
+// Movies reducer and action generators
+// -----------------------------------
+var nextMovieId = 1;
+var moviesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'MOVIE_ADD':
+    return [
+      ...state,
+      {
+        id: nextMovieId++,
+        title: action.title,
+        genrer: action.genrer
+      }
+    ];
+    case 'REMOVE_MOVIE':
+      return state.filter((movie) => movie.id !== action.id)
+    default:
+      return state;
+      }
+};
+
+var addMovie = (title, genrer) => {
+  return {
+    type: 'MOVIE_ADD',
+    title,
+    genrer
+  };
+};
+
+var removeMovie = (id) => {
+  return {
+    type: 'REMOVE_MOVIE',
+    id
+  };
+};
+
+
+var reducer = redux.combineReducers({
+  name: nameReducer,
+  hobbies: hobbiesReducer,
+  movies: moviesReducer
+})
+
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => { // takes a function u would like to call everytime ur state changes
+   var state = store.getState();
+   console.log('New state', store.getState());
+   document.getElementById('app').innerHTML = state.name;
+});
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Andrew'
+};
+store.dispatch(action);
+// unsubscribe(); // will unsubscribe Jennifer and Edward.
+
+store.dispatch(changeName('Anastacia'));
+store.dispatch(changeName('Sonique'));
+store.dispatch(addHobby('Rugby'));
+store.dispatch(addHobby('Swimming'));
+store.dispatch(addMovie('Gone girl', 'fun fun'));
+store.dispatch(addMovie('Loopers', 'action'));
+store.dispatch(addMovie('The passion', 'religion'));
+store.dispatch(removeMovie(2));
+store.dispatch(removeHobby(2));
+
+
+// store.dispatch({
+//   type: 'CHANGE_NAME',
+//   name: 'Jennifer'
+// });
+//
+// store.dispatch({
+//   type: 'ADD_HOBBY',
+//   hobby: 'fishing'
+// });
+// store.dispatch({
+//   type: 'ADD_HOBBY',
+//   hobby: 'Running'
+// });
+//
+// store.dispatch({
+//   type: 'MOVIE_ADD',
+//   title: 'Slumber party',
+//   genrer: 'fantasy'
+// });
+// store.dispatch({
+//   type: 'MOVIE_ADD',
+//   title: 'Loopers',
+//   genrer: 'action'
+// });
+// // store.dispatch({
+// //   type: 'REMOVE_HOBBY',
+// //   id: 2
+// // });
+// store.dispatch({
+//   type: 'REMOVE_MOVIE',
+//   id: 1
+// });
+
+
+
+
+
+// var stateDefault = {
+//   name: 'Anonymous',
+//   hobbies: [],
+//   movies: []
+// };
 // var oldreducer = (state = stateDefault, action) => {
 //    switch (action.type) {
 //     case 'CHANGE_NAME':
@@ -54,113 +217,3 @@ var nextMovieId = 1;
 //       return state;
 //   }
 // };
-
-
-
-var nameReducer = (state = 'Anonymous', action) => {
-  switch (action.type) {
-    case 'CHANGE_NAME':
-    return action.name
-    default:
-    return state;
-  };
-};
-
-var hobbiesReducer = (state =[], action) => {
-  switch (action.type) {
-    case 'ADD_HOBBY':
-    return [
-      ...state, // all the existing hobbies.
-      {
-        id: nextHobbyId++,
-        hobby: action.hobby
-      }
-    ];
-    case 'REMOVE_HOBBY':
-      return state.filter((hobby) => hobby.id !== action.id)
-    default:
-      return state;
-  }
-};
-
-var moviesReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'MOVIE_ADD':
-    return [
-      ...state,
-      {
-        id: nextMovieId++,
-        title: action.title,
-        genrer: action.genrer
-      }
-    ];
-    case 'REMOVE_MOVIE':
-      return state.filter((movie) => movie.id !== action.id)
-    default:
-      return state;
-  }
-
-};
-
-var reducer = redux.combineReducers({
-  name: nameReducer,
-  hobbies: hobbiesReducer,
-  movies: moviesReducer
-})
-
-var store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-));
-
-// Subscribe to changes
-var unsubscribe = store.subscribe(() => { // takes a function u would like to call everytime ur state changes
-   var state = store.getState();
- //console.log('Name is', state.name);
-//console.log('New state', state.hobbies);// to b able to see everything that changes in the state.
-  console.log('New state', store.getState());
-  document.getElementById('app').innerHTML = state.name;
-});
-
-
-var action = {
-  type: 'CHANGE_NAME',
-  name: 'Andrew'
-};
-store.dispatch(action);
-// unsubscribe(); // will unsubscribe Jennifer and Edward.
-
-store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'Jennifer'
-});
-store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'Edward'
-});
-store.dispatch({
-  type: 'ADD_HOBBY',
-  hobby: 'fishing'
-});
-store.dispatch({
-  type: 'ADD_HOBBY',
-  hobby: 'Running'
-});
-
-store.dispatch({
-  type: 'MOVIE_ADD',
-  title: 'Slumber party',
-  genrer: 'fantasy'
-});
-store.dispatch({
-  type: 'MOVIE_ADD',
-  title: 'Loopers',
-  genrer: 'action'
-});
-store.dispatch({
-  type: 'REMOVE_HOBBY',
-  id: 2
-});
-store.dispatch({
-  type: 'REMOVE_MOVIE',
-  id: 1
-});
